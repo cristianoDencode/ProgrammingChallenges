41fa3cfed55e9a5e789132035d3dc00aadaedefd
cristianoDencode <cristiano.aquinorj@hotmail.com>
2026-01-27 07:08:31 -0300

    feat: Initial Adjustment in index

--- Challenges/Observer/SolutionV1/index.php
+++ Challenges/Observer/SolutionV1/index.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 namespace Observer\SolutionV1;
 
-echo 'está no v1';
+require_once __DIR__.'/../../vendor/autoload.php';
+use Observer\SolutionV1\Enrollment\Enrollment;
+
+try {
+    $enrollment = new Enrollment('student1@email.com', 'Arquitetura de Software');
+    $enrollment->confirm();
+    $enrollment->activate();
+    $enrollment->cancel();
+    echo '<br>';
+    var_dump($enrollment);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
